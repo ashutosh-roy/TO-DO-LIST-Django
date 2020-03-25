2e0f9421c07e4ec2ca9af268bf56c33719e44753
dopecoder007 <dopecoder007@gmail.com>
2020-03-26 01:38:00 +0530

    Added .gitignore into directory for cherry-picking

--- register/models.py
+++ register/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class detail(models.Model):
+    email= models.CharField(max_length=200)
+    phone = models.IntegerField()
+    country = models.CharField(max_length=200) 
+    created = models.DateTimeField(auto_now_add = True)  
+
+    def __str__(self):
+        return self.email
--- register/views.py
+++ register/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import detail
 
 def index(request):
-    return HttpResponse("Hello World..")
+    #return HttpResponse("Hello World..")
+    d = detail.objects.all()
+    context = {'d':d}
+    return render(request,'voluntaria/register.html',context)
