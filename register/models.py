1087548b2b9c11a798e0cb210169bb884ad3778a
dopecoder007 <dopecoder007@gmail.com>
2020-03-26 03:47:21 +0530

    Remove ignored files

--- register/models.py
+++ register/models.py
@@ -7,5 +7,7 @@ class detail(models.Model):
     country = models.CharField(max_length=200) 
     created = models.DateTimeField(auto_now_add = True)  
 
+    #This function send the value to the template in which it has been called 
+    #In this case it has returned "email"
     def __str__(self):
         return self.email
--- register/views.py
+++ register/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import detail
+
+#importing the details wala table from the class 'model'
+from .models import *
+from .forms import *
 
 def index(request):
-    #return HttpResponse("Hello World..")
+    # Stores the value returned by this function
     d = detail.objects.all()
-    context = {'d':d}
+    form= detailForm()
+    
+    """ Its a dictionary that stores key : value pairs 
+    Syntax {'variable___storing___the___date':variable___storing___the___date}"""
+    context = {'d':d,'form':form}
+    
+    # The context is passed to the given HTML Template file 
     return render(request,'voluntaria/register.html',context)
